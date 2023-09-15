edef827b3e2f4e354a36c7453aaa17461c67b2b1
Jean Carlo <jeansanto2002@gmail.com>
2023-09-15 14:05:26 -0300

    trabalhando com arrays e listas

---
+++ 7.Arrays/trabalhando_com_arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] arrayInteiro = new int[4];
+
+arrayInteiro[0] = 10;
+arrayInteiro[1] = 20;
+arrayInteiro[2] = 30;
+arrayInteiro[3] = 40;
+
+// Copiando um array para outro
+int[] arrayInteiroDobrado = new int[arrayInteiro.Length * 2];
+Array.Copy(arrayInteiro, arrayInteiroDobrado, arrayInteiro.Length);
+Console.WriteLine(arrayInteiroDobrado);
+
+// REDIMENSIONAMENTO
+// Array.Resize(ref arrayInteiro, arrayInteiro.Length * 2);
+
+Console.WriteLine("Percorrendo o array com o FOR");
+for(int contador = 0; contador < arrayInteiro.Length; contador++ ){
+    Console.WriteLine($"Posição Nº {contador} - {arrayInteiro[contador]}");
+}
+
+// Console.WriteLine();
+// Console.WriteLine("Percorrendo o array com o FOREACH");
+// int contadorForeach = 0;
+// foreach(int valor in arrayInteiro){
+//     Console.WriteLine($"Posição Nº {contadorForeach} - {valor}");
+//     contadorForeach++;
+// }
---
+++ 7.Arrays/trabalhando_com_listas/Program.cs
@@ -0,0 +1,39 @@
+﻿List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("RJ");
+listaString.Add("ES");
+listaString.Add("MS");
+
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Add("SC");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Remove("ES");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("FOR");
+// for(int contador =0; contador < listaString.Count; contador++){
+//     Console.WriteLine($"Posição Nº {contador} - {listaString[contador]}");
+// }
+
+// Console.WriteLine("");
+
+// Console.WriteLine("FOREACH");
+// int contadorForeach = 0;
+// foreach(string item in listaString){
+//    Console.WriteLine($"Posição Nº {contadorForeach} - {item}");
+//    contadorForeach++;
+// }
